e9b35ac304aa447104a0b72970fb6b18ef5a3ed6
ToastedGMS <gabrielsiqueira3052@gmail.com>
2026-04-30 11:51:04 -0300

    feat: implementar estrutura de classes (Herança e Polimorfismo)

---
+++ src/Colaborador.java
@@ -0,0 +1,49 @@
+public class Colaborador {
+    String nomeCompleto;
+    String numeroRegistro;
+    final double salarioBase = 2000; // utiliza final para evitar modificação do valor
+
+    double calcularSalario() {
+        return salarioBase;
+    }
+
+    public Colaborador(String nome, String registro) { // construtor
+        this.nomeCompleto = nome;
+        this.numeroRegistro = registro;
+    }
+}
+
+class ColaboradorComissionado extends Colaborador {
+    private double valorVendas;
+    private double comissaoPercentual;
+
+    @Override // utiliza override para evitar o uso de condicionais longas e repetitivas
+    double calcularSalario() {
+        double valorComissao = valorVendas * comissaoPercentual / 100;
+        return salarioBase + valorComissao;
+    }
+
+    public ColaboradorComissionado(String nome, String registro, double vendas, double comissao) {
+        super(nome, registro); // super garante que o construtor chama os metodos da classe pai antes de
+                               // aplicar os seus
+        this.valorVendas = vendas;
+        this.comissaoPercentual = comissao;
+    }
+}
+
+class ColaboradorProducao extends Colaborador {
+    private double quantidadePecas;
+    private double valorPeca;
+
+    @Override
+    double calcularSalario() {
+        double bonusProdutividade = valorPeca * quantidadePecas;
+        return salarioBase + bonusProdutividade;
+    }
+
+    public ColaboradorProducao(String nome, String registro, double pecas, double valor) {
+        super(nome, registro);
+        this.quantidadePecas = pecas;
+        this.valorPeca = valor;
+    }
+}
