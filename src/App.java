71b344c03c86694629e427bc0ba0ca912b3e90be
ToastedGMS <gabrielsiqueira3052@gmail.com>
2026-05-02 14:11:25 -0300

    feat: adicionar funcionalidade de cadastro de colaborador padrão no menu

--- src/App.java
+++ src/App.java
@@ -5,14 +5,29 @@ public class App {
     public static void main(String[] args) {
         ArrayList<Colaborador> colaboradores = new ArrayList<Colaborador>();
         Scanner sc = new Scanner(System.in);
-        int opcao = sc.nextInt();
+        int opcao; // declara a variavel fora do loop para que ela nao seja criada novamente em
+                   // cada execução
 
         do {
             System.out.printf(
                     "Digite sua escolha:\n1-Cadastrar Funcionario Padrao\n2-Cadastrar Funcionario Comissionado\n3-Cadastrar Funcionario Producao\n4-Gerar Folha de Pagamento\n0-Sair do Programa");
-
+            opcao = sc.nextInt();
             switch (opcao) {
                 case 1:
+                    sc.nextLine();
+                    System.out.println("Cadastro de colaborador padrão");
+                    System.out.println("Informe o nome do colaborador:");
+                    String nomeColaboradorPadrao = sc.nextLine();
+
+                    System.out.println("Informe o número de registro do colaborador:");
+                    String registroColaboradorPadrao = sc.nextLine();
+
+                    Colaborador newColaborador = new Colaborador(nomeColaboradorPadrao, registroColaboradorPadrao);
+                    colaboradores.add(newColaborador);
+                    System.out.println("Colaborador " + newColaborador.nomeCompleto + " adicionado.");
+
+                    System.out.println("\n[Pressione ENTER para voltar ao menu]");
+                    sc.nextLine(); // opção de melhoria para UX
                     break;
                 case 2:
                     break;
@@ -21,6 +36,7 @@ public class App {
                 case 4:
                     break;
                 case 0:
+                    System.out.println("Até mais!");
                     break;
                 default:
                     break;
