ab049757e6528576732bb6069d9ecedaa17c6a33
ToastedGMS <gabrielsiqueira3052@gmail.com>
2026-05-02 14:39:45 -0300

    feat: implementar geração da folha de pagamento com detalhes dos colaboradores

--- src/App.java
+++ src/App.java
@@ -84,11 +84,37 @@ public class App {
                     sc.nextLine();
                     break;
                 case 4:
+                    System.out.println("Gerar folha de pagamento");
+                    System.out.println("Total de funcionários: " + colaboradores.size());
+
+                    colaboradores.forEach(item -> {
+                        System.out.println("Nome: " + item.nomeCompleto);
+                        System.out.println("Matrícula: " + item.numeroRegistro);
+                        System.out.println("Salário Base: " + item.salarioBase);
+                        if (item instanceof ColaboradorComissionado) {
+                            ColaboradorComissionado c = (ColaboradorComissionado) item; // como o compilador espera uma
+                                                                                        // lista de itens do tipo
+                                                                                        // Colaborador, é preciso criar
+                                                                                        // uma nova variável do tipo
+                                                                                        // adequado, mas que contenha os
+                                                                                        // mesmos dados da variável
+                                                                                        // original
+                            System.out.println("Valor Comissão: " + c.getValorComissao());
+                        } else if (item instanceof ColaboradorProducao) {
+                            ColaboradorProducao p = (ColaboradorProducao) item;
+                            System.out.println("Bônus Produtividade: " + p.bonusProdutividade);
+                        }
+                        System.out.println("Salário Total: " + item.calcularSalario());
+                    });
+
+                    System.out.println("\n[Pressione ENTER para voltar ao menu]");
+                    sc.nextLine();
                     break;
                 case 0:
                     System.out.println("Até mais!");
                     break;
                 default:
+                    System.out.println("Opção inválida! Por favor, digite um número entre 0 e 4.");
                     break;
             }
         } while (opcao != 0);
